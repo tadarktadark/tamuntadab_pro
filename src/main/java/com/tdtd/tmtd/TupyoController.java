5a47f8ef1c4df7d6937593887613fcc49325c7c7
dahyun623 <danielanderic@gmail.com>
2023-09-08 14:25:49 +0900

    TDTD-214 main 페이지 admin, tuypo 링크 추가

--- src/main/java/com/tdtd/tmtd/TupyoController.java
+++ src/main/java/com/tdtd/tmtd/TupyoController.java
@@ -1,8 +1,24 @@
 package com.tdtd.tmtd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tdtd.tmtd.model.service.ITupyoService;
+import com.tdtd.tmtd.vo.TupyoVo;
 
 @Controller
 public class TupyoController {
+	
+	@Autowired
+	private ITupyoService service;
+	
+	@RequestMapping(value = "/tupyoPage.do")
+	public String tupyoPage(Model model) {
+		TupyoVo vo = service.getTupyo(1000000033);
+		model.addAttribute("vo",vo);
+		return "tupyo";
+	}
 
 }
