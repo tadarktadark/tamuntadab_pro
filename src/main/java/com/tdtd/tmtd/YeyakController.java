90b40ad9e9e6ca51e772b164f663b2e33355bea3
s5hy25ni <s5hy25ni@gmail.com>
2023-09-10 03:40:09 +0900

    TDTD-143 강의실 목록 페이징 완료

--- src/main/java/com/tdtd/tmtd/YeyakController.java
+++ src/main/java/com/tdtd/tmtd/YeyakController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tdtd.tmtd.comm.PagingUtils;
 import com.tdtd.tmtd.model.service.IYeyakService;
 import com.tdtd.tmtd.vo.GangeuisilVo;
 
@@ -26,19 +27,23 @@ public class YeyakController {
 	@RequestMapping(value="/yeyak.do", method = RequestMethod.GET)
 	public String yeyak(Model model) {
 		log.info("@@@@@@@@@@@@@@@ 예약 페이지 이동");
-		int count = service.getGangeuisilCount(null);
-		List<String> sdList = service.getGangeuisilSidoList();
-		Map<String, Object> map = new HashMap<String, Object>(){{
-			put("start", 1);
-			put("end", 5);
-		}};
-		List<GangeuisilVo> glist = service.getGangeuisilList(map);
-		model.addAttribute("sidoCount", count);
-		model.addAttribute("sido", sdList);
-		model.addAttribute("comm", glist);
 		model.addAttribute("title","예약");
 		model.addAttribute("pageTitle", "예약");
 		return "yeyak";
+	}
+	
+	@RequestMapping(value="/getGangeuisilSidoList.do", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> getGangeuisilSidoList() {
+		log.info("@@@@@@@@@@@@@@@ 시도 강의실 목록 조회");
+		int count = service.getGangeuisilCount(null);
+		Map<String, Object> pMap = PagingUtils.paging("1", count, 5, 5); 
+		Map<String, Object> result = new HashMap<String, Object>(){{
+			put("list", service.getGangeuisilSidoList());
+			put("count", count);
+			put("page", pMap.get("page"));
+		}};
+		return result;
 	}
 	
 	@RequestMapping(value="/getGangeuisilSigunguList.do", method = RequestMethod.GET)
@@ -59,22 +64,28 @@ public class YeyakController {
 	
 	@RequestMapping(value="/getGangeuisilList.do", method=RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> getGangeuisilList(String type, String sendData){
-		log.info("@@@@@@@@@@@@@@@ 공통 강의실 목록 조회 : {}, {}", type, sendData);
-		Map<String, Object> map = new HashMap<String, Object>(){{
-			put("start", 1);
-			put("end", 5);
-		}};
+	public Map<String, Object> getGangeuisilList(String page, String type, String sendData){
+		log.info("@@@@@@@@@@@@@@@ 공통 강의실 목록 조회 :{}, {}, {}", page, type, sendData);
+		Map<String, Object> map = new HashMap<String, Object>();
 		if(type.equals("sido")) {
 			map.put("gacoSido", sendData);
 		} else if(type.equals("sigungu")){
 			map.put("gacoSigungu", sendData);
 		}
 		int count = service.getGangeuisilCount(map);
+		Map<String, Object> pMap = PagingUtils.paging(page, count, 5, 5);
+		map.put("start", pMap.get("start") );
+		map.put("end", pMap.get("end"));
 		List<GangeuisilVo> list = service.getGangeuisilList(map);
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		Map<String, Object> result = new HashMap<String, Object>(){{
 			put("count", count);
 			put("list", list);
+			put("page", pMap.get("page"));
 		}};
 		return result;
 	}
--- src/main/webapp/js/yeyak.js
+++ src/main/webapp/js/yeyak.js
@@ -1,53 +1,58 @@
 $(document).ready(function(){
-	$(".sido-dropdown").click(function(e){
-		$("#btn-sigungu").remove();
-		$("#sido-title").text(e.target.text);			
-		getGangeuisilList("sido", e.target.text.substr(0,2));
-		if(e.target.text.substr(0,2)!="전체"){
-			$("#sido-title").removeClass("btn-primary");
-			$("#sido-title").addClass("btn-light");
-			$.ajax({
-				type:"get",
-				url:"./getGangeuisilSigunguList.do",
-				data:{
-					"sido":e.target.text.substr(0,2)
-				},
-				success: function(data){
-					$("#btn-sido").after('<div class="btn-group" id="btn-sigungu">'+
-										    '<button id="sigungu-title" class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-										        '전체('+data.count+')'+
-										    '</button>'+
-										    '<div class="dropdown-menu dropdownmenu-primary sigungu-group" id="sigungu-group" data-simplebar data-simplebar-track="primary">')
-					for(var i=0; i<data.list.length; i++){
-						$("#sigungu-group").append('<a class="dropdown-item sigungu-dropdown click-btn">'+data.list[i]+'</a>'
-										)
-					}
-					$("#btn-sigungu").after('</div>'+
-										'</div>')
-				},
-				error: function(){
-					alert("잘못된 요청입니다.");
-				}
-			});
-		} else {
-			$("#sido-title").removeClass("btn-light");
-			$("#sido-title").addClass("btn-primary");
-			getGangeuisilList("all", e.target.text.substr(0,2));
-		}		
-	});
+	getGangeuisilSidoList();
+	getGangeuisilList("1","all", "전체");
 });
 
+$(document).on('click', '.sido-dropdown', function(e){
+	$("#multiCollapseExample2").removeClass("show");
+	$("#btn-sigungu").remove();
+	$("#sido-title").text(e.target.text);			
+	if(e.target.text.substr(0,2)!="전체"){
+		$("#sido-title").removeClass("btn-primary");
+		$("#sido-title").addClass("btn-light");
+		getGangeuisilList("1","sido", e.target.text.substr(0,2));
+		$.ajax({
+			type:"get",
+			url:"./getGangeuisilSigunguList.do",
+			data:{
+				"sido":e.target.text.substr(0,2)
+			},
+			success: function(data){
+				$("#btn-sido").after('<div class="btn-group" id="btn-sigungu">'+
+									    '<button id="sigungu-title" class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+									        '전체('+data.count+')'+
+									    '</button>'+
+									    '<div class="dropdown-menu dropdownmenu-primary sigungu-group" id="sigungu-group" data-simplebar data-simplebar-track="primary">')
+				for(var i=0; i<data.list.length; i++){
+					$("#sigungu-group").append('<a class="dropdown-item sigungu-dropdown click-btn">'+data.list[i]+'</a>'
+									)
+				}
+				$("#btn-sigungu").after('</div>'+
+									'</div>')
+			},
+			error: function(){
+				alert("잘못된 요청입니다.");
+			}
+		});
+	} else {
+		$("#sido-title").removeClass("btn-light");
+		$("#sido-title").addClass("btn-primary");
+		getGangeuisilList("1","all", e.target.text.substr(0,2));
+	}
+});
 $(document).on('click', '.sigungu-dropdown', function(e) {
+	$("#multiCollapseExample2").removeClass("show");
 	$("#sigungu-title").text(e.target.text);
 	if(e.target.text.substr(0,2)!="전체"){
-		getGangeuisilList("sigungu", e.target.text.substr(0,e.target.text.indexOf("(")));
+		getGangeuisilList("1","sigungu", e.target.text.substr(0,e.target.text.indexOf("(")));
 	} else {
-		getGangeuisilList("sido", $("#sido-title").text().substr(0,2));
+		getGangeuisilList("1","sido", $("#sido-title").text().substr(0,2));
 	}		
 
 });
 
 $(document).on('click', '.show-gangeuisilDetails', function(e) {
+	$("#multiCollapseExample2").addClass("show");
 	$.ajax({
 		type:"get",
 		url:"./getGangeuisilDetailList.do",
@@ -58,7 +63,7 @@ $(document).on('click', '.show-gangeuisilDetails', function(e) {
 			$("#"+data[0].gacoId).children().remove();
 			$("#"+data[0].gacoId).append('<tr>'+
 								            '<th scope="col"><i class="ri-home-wifi-fill"></i>&nbsp;강의실</th>'+
-								            '<th scope="col"><i class="ri-group-line"></i>&nbsp;최대 인원</th>'+
+								            '<th scope="col"><i class="ri-group-line"></i>&nbsp;최대</th>'+
 								            '<th scope="col"><i class="ri-hand-coin-line"></i>&nbsp;1시간</th>'+
 								            '<th scope="col"></th>'+
 								    	'</tr>');
@@ -77,12 +82,65 @@ $(document).on('click', '.show-gangeuisilDetails', function(e) {
 		}
 	});
 });
-
-function getGangeuisilList(type, sendData){
+$(document).on('click', '.page-link', function(e) {
+	$("#multiCollapseExample2").removeClass("show");
+	var sido = $("#sido-title").text().substr(0,2);
+	var sigungu = $("#sigungu-title").text().substr(0,$("#sigungu-title").text().indexOf("("));
+	if(sido == "전체"){		
+		getGangeuisilList(e.target.id, "all", "전체");
+	} else if(sigungu == "전체" || sigungu == ""){
+		getGangeuisilList(e.target.id, "sido", sido);
+	} else {
+		getGangeuisilList(e.target.id, "sigungu", sigungu);		
+	}
+});
+function getGangeuisilSidoList(){
+	$.ajax({
+		type:"get",
+		url:"./getGangeuisilSidoList.do",
+		success: function(data){
+			var html = '<div class="btn-group" id="btn-sido">'+
+					    '<button id="sido-title" class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+					        '전체('+data.count+')'+
+					    '</button>'+
+					    '<div class="dropdown-menu dropdownmenu-primary sido-group" data-simplebar data-simplebar-track="primary">'+
+					        '<a class="dropdown-item sido-dropdown click-btn">전체('+data.count+')</a>';
+							for(var i=0; i<data.list.length;i++){
+						        html+='<a class="dropdown-item sido-dropdown click-btn">'+data.list[i]+'</a>';								
+							}				
+				html +=	'</div>'+
+					'</div>'+
+					'<br>'+
+					'<br>'+
+					'<div class="row">'+
+					    '<div class="col">'+
+	 						'<div class="accordion custom-accordionwithicon-plus collapse multi-collapse show" id="accordionWithplusicon">'+
+							'</div>'+
+							'<br>'+
+							'<ul id="pagination" class="pagination pagination-rounded justify-content-center">'+
+							'</ul>'+
+					    '</div>'+
+					    '<div class="col">'+
+					        '<div class="collapse multi-collapse" id="multiCollapseExample2">'+
+					            '<div class="card card-body mb-0" id="map">'+
+					            '</div>'+
+					        '</div>'+
+					    '</div>'+
+					'</div>';
+			$("#yeyak-container").append(html);
+			changePage(data.page.startPage, data.count, data.page.endPage, data.page.page);
+		},
+		error: function(){
+			alert("잘못된 요청입니다.");
+		}
+	});		
+}
+function getGangeuisilList(page, type, sendData){
 	$.ajax({
 		type:"post",
 		url:"./getGangeuisilList.do",
 		data:{
+			"page":page,
 			"type":type,
 			"sendData":sendData
 		},
@@ -119,9 +177,29 @@ function getGangeuisilList(type, sendData){
 												        '</div>'+
 												    '</div>');
 			}
+			changePage(data.page.startPage, data.count, data.page.endPage, data.page.page);
 		},
 		error: function(){
 			alert("잘못된 요청입니다.");
 		}
 	});	
+}
+
+function changePage(start, count, end, page){
+	$("#pagination").children().remove()
+	var html =	'<li class="page-item">'+
+		        '<a id="'+(start-count)+'" class="page-link click-btn"><i class="mdi mdi-chevron-left"></i></a>'+
+		    '</li>';
+		for(var i=start; i<=end;i++){
+			html +=
+			    '<li class="page-item '+((i==page)?"active":"")+'">'+
+			        '<a id="'+i+'" class="page-link click-btn">'+i+'</a>'+
+			    '</li>'									
+			;
+		}
+		    html +=
+		    '<li class="page-item">'+
+		        '<a id="'+(start+count)+'"class="page-link click-btn"><i class="mdi mdi-chevron-right"></i></a>'+
+		    '</li>';
+	$("#pagination").append(html);
 }
