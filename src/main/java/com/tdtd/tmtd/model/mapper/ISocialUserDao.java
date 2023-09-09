97f4c0730f9d0e7990158dfa3c2a22f527f75397
juoju <juoju@wjddns.mshome.net>
2023-09-09 20:38:02 +0900

    TDW62100 회원가입 페이지 구현 진행중

--- src/main/java/com/tdtd/tmtd/model/mapper/ISocialUserDao.java
+++ src/main/java/com/tdtd/tmtd/model/mapper/ISocialUserDao.java
@@ -1,5 +1,40 @@
 package com.tdtd.tmtd.model.mapper;
 
-public interface ISocialUserDao {
+import java.util.Map;
 
+public interface ISocialUserDao {
+	
+	/**
+	* NOTE 소셜 회원에 대한 기능을 담은 인터페이스
+	* @author : 임정운
+	* @since : 2023.09.09
+	* @version : 1.0
+	*/
+	
+	/**
+	* NOTE 입력 값을 받아 해당 입력 값을 NAVER_USER_PROFILE DB에 저장시키는 메소드
+	* @param : 사용자가 입력한 값 / 타입 : Map<String,Object>
+	* @return : 1 == 입력 성공 0 == 입력 실패 / 타입 : int
+	* @author : 임정운
+	* @since : 2023.09.09
+	*/
+	public int registNaverUser(Map<String,Object> userProfile);
+	
+	/**
+	* NOTE 입력 값을 받아 해당 입력 값을 KAKAO_USER_PROFILE DB에 저장시키는 메소드
+	* @param : 사용자가 입력한 값 / 타입 : Map<String,Object>
+	* @return : 1 == 입력 성공 0 == 입력 실패 / 타입 : int
+	* @author : 임정운
+	* @since : 2023.09.09
+	*/
+	public int registKakaoUser(Map<String,Object> userProfile);
+	
+	/**
+	* NOTE 입력 값을 받아 해당 입력 값을 GOOGLE_USER_PROFILE DB에 저장시키는 메소드
+	* @param : 사용자가 입력한 값 / 타입 : Map<String,Object>
+	* @return : 1 == 입력 성공 0 == 입력 실패 / 타입 : int
+	* @author : 임정운
+	* @since : 2023.09.09
+	*/
+	public int registGoogleUser(Map<String,Object> userProfile);
 }
