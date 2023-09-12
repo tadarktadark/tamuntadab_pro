89e9ca53521853be9d2125482c58d697ce0669eb
hamoon1028 <mho1028@gmail.com>
2023-09-12 11:17:35 +0900

    커밋

--- src/main/java/com/tdtd/tmtd/model/service/IFileService.java
+++ src/main/java/com/tdtd/tmtd/model/service/IFileService.java
@@ -1,5 +1,24 @@
 package com.tdtd.tmtd.model.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface IFileService {
 
+	 public Map<String, Object> fileSave(MultipartFile file, HttpServletRequest request) throws IOException;
+	 
+	 public File fileDownload(HttpServletRequest request, String saveFileName) throws IOException;
+	 
+	 public List<String> convertPdfToPng(String pdfFilePath, String outputDir) throws Exception;
+	 
+	 public Map<String,Object> extractTextFromAreas(String fileName);
+	 
+	 public void deletePngFiles(List<String> pngFileNames, String directory);
+	
 }
