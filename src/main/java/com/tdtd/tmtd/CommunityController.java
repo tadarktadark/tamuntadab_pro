6fe2d26a2e8d3d6fe7f8be657d1f6bffeaef493d
s5hy25ni <s5hy25ni@gmail.com>
2023-09-17 13:27:59 +0900

    TDTD-127 커뮤니티 목록 완료

--- src/main/java/com/tdtd/tmtd/CommunityController.java
+++ src/main/java/com/tdtd/tmtd/CommunityController.java
@@ -58,43 +58,74 @@ public class CommunityController {
 		return "communityList";
 	}
 	
-	@RequestMapping(value="/getPilgiList.do", method=RequestMethod.POST)
+	@RequestMapping(value="/getCommunityList.do", method=RequestMethod.POST)
 	@ResponseBody
-	public Map<String,Object> getPilgiList(HttpSession session, String orderBy, String page){
-		log.info("@@@@@@@@@@@@@@@ 필기 목록 조회 : {}, {}", orderBy, page);
+	public Map<String,Object> getCommunityList(HttpSession session, String orderBy, String page){
+		String board = (String)session.getAttribute("community");
+		log.info("@@@@@@@@@@@@@@@ 커뮤니티 목록 조회 : board {}, orderBy {}, page {}", board, orderBy, page);
 		
 		UserProfileVo userInfo = (UserProfileVo)session.getAttribute("userInfo");
-		
 		if(userInfo == null) {
 			userInfo = new UserProfileVo();
 			userInfo.setUserAccountId("TMTD0");
 		}
 		
-		int pageCount = pService.getPilgiCount(userInfo.getUserAccountId());			
-		Map<String, Object> pMap = PagingUtils.paging(page, pageCount, 10, 5);
-		
-		Map<String, Object> bMap = new HashMap<String, Object>(){{
-			put("orderBy", orderBy);
-			put("start", pMap.get("start"));
-			put("end", pMap.get("end"));
-		}};
+		int pageCount; // 전체 게시글 수
+		Map<String, Object> pMap = new HashMap<String, Object>(); // page 객체 및 start, end
+		Map<String, Object> bMap = new HashMap<String, Object>(); // board 관련 accountId, orderBy, start, end
 		bMap.put("accountId", userInfo.getUserAccountId());
+		bMap.put("orderBy", orderBy);
 		
-		Map<String, Object> result = new HashMap<String, Object>(){{
-			put("b",pService.getPilgiList(bMap));
-			put("p", pMap.get("page"));
-		}};
+		if(board.equals("pilgi")) {
+			pageCount = pService.getPilgiCount(userInfo.getUserAccountId());
+			pMap = PagingUtils.paging(page, pageCount, 10, 5);
+		} else if(board.equals("jilmun")) {
+			pageCount = jmService.getJilmunCount();
+			pMap = PagingUtils.paging(page, pageCount, 10, 5);
+		} else if(board.equals("jayu")) {
+			pageCount = jyService.getJayuCount();
+			pMap = PagingUtils.paging(page, pageCount, 10, 5);
+		}
+		
+		bMap.put("start", pMap.get("start"));
+		bMap.put("end", pMap.get("end"));
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("page", pMap.get("page"));
+		
+		if(board.equals("pilgi")) {
+			result.put("board", pService.getPilgiList(bMap));
+		} else if(board.equals("jilmun")) {
+			result.put("board", jmService.getJilmunList(bMap));
+		} else if(board.equals("jayu")) {
+			result.put("board", jyService.getJayuList(bMap));
+		}
+		
 		return result;
 	}
 	
-	@RequestMapping(value="/pilgiLikeUser.do", method=RequestMethod.POST)
+	@RequestMapping(value="/commynityLike.do", method=RequestMethod.POST)
 	@ResponseBody
-	public Map<String,Object> pilgiLikeUser(HttpSession session, String type, String id){
-		log.info("@@@@@@@@@@@@@@@ 필기 좋아요 : {}, {}", type, id);
+	public Map<String,Object> commynityLike(HttpSession session, String type, String id){
+		String board = (String)session.getAttribute("community");
+		log.info("@@@@@@@@@@@@@@@ 커뮤니티 좋아요 : board {}, type {}, id {}", board, type, id);
 		
 		UserProfileVo userInfo = (UserProfileVo)session.getAttribute("userInfo");
+		if(userInfo == null) {
+			userInfo = new UserProfileVo();
+			userInfo.setUserAccountId("TMTD0");
+		}
 		
-		String list = pService.getPilgiLikeUser(id);		
+		String list = "";
+		
+		if(board.equals("pilgi")) {
+			list = pService.getPilgiLikeUser(id);
+		} else if(board.equals("jilmun")) {
+			list = jmService.getJilmunLikeUser(id);
+		} else if(board.equals("jayu")) {
+			list = jyService.getJayuLikeUser(id);
+		}
+		
 		Map<String, Object> like = LikeViewUtils.like(type, userInfo.getUserAccountId(), list);
 		if((int)like.get("update")==1) {
 			Map<String, Object> data = new HashMap<String, Object>(){{
@@ -102,8 +133,15 @@ public class CommunityController {
 				put("likeCount", like.get("count"));
 				put("id", id);
 			}};
-			pService.updatePilgiLikeUser(data);
+			if(board.equals("pilgi")) {
+				pService.updatePilgiLikeUser(data);
+			} else if(board.equals("jilmun")) {
+				jmService.updateJilmunLikeUser(data);
+			} else if(board.equals("jayu")) {
+				jyService.updateJayuLikeUser(data);
+			}
 		}
+		
 		Map<String, Object> result = new HashMap<String, Object>(){{
 			put("type",like.get("type"));
 			put("count",like.get("count"));
@@ -111,39 +149,10 @@ public class CommunityController {
 		return result;
 	}
 	
-	@RequestMapping(value="/getPilgiDetail.do", method=RequestMethod.GET)
-	public String getPilgiDetail(HttpSession session, Model model, String id){
-		log.info("@@@@@@@@@@@@@@@ 커뮤니티 상세조회 : {}", id);
-		
-		UserProfileVo userInfo = (UserProfileVo)session.getAttribute("userInfo");
-		
-		Map<String, Object> detail = new HashMap<String, Object>();
-		detail.put("accountId",userInfo.getUserAccountId());
-		detail.put("id",id );
-		
-		System.out.println(detail);
-		
-		BoardVo bVo = pService.getPilgiDetail(detail);
-		
-		String str = bVo.getSubjectCode().substring(1, bVo.getSubjectCode().length() - 1); // 대괄호 제거
-
-		String[] array = str.split(","); // 쉼표를 기준으로 분리
-
-		for (int i = 0; i < array.length; i++) {
-		    array[i] = array[i].replaceAll("\"", "").trim(); // 큰따옴표 제거 및 공백 제거
-		}
-		
-		model.addAttribute("title","커뮤니티");
-		model.addAttribute("pageTitle", "필기");
-		model.addAttribute("bVo", bVo);
-		model.addAttribute("yList", pService.getYeongwanList(detail));
-		model.addAttribute("subArr", array);
-		return "pilgiDetails";
-	}
-	
-	@RequestMapping(value="/commynityWriteForm.do", method=RequestMethod.GET)
-	public String commynityWriteForm(Model model, HttpSession session, String id) {
-		log.info("@@@@@@@@@@@@@@@ 커뮤니티 새 글 작성 Form 이동 : board {}, boardId {}", session.getAttribute("community"), id);
+	@RequestMapping(value="/communityDetails.do", method=RequestMethod.GET)
+	public String communityDetails(HttpSession session, Model model, String id){
+		String board = (String)session.getAttribute("community");
+		log.info("@@@@@@@@@@@@@@@ 커뮤니티 상세조회 : board {}, id {}", board, id);
 		model.addAttribute("title","커뮤니티");
 		
 		UserProfileVo userInfo = (UserProfileVo)session.getAttribute("userInfo");
@@ -152,7 +161,53 @@ public class CommunityController {
 			userInfo.setUserAccountId("TMTD0");
 		}
 		
+		Map<String, Object> detail = new HashMap<String, Object>();
+		detail.put("accountId",userInfo.getUserAccountId());
+		detail.put("id",id );
+		
+		BoardVo bVo = new BoardVo();
+		
+		if(board.equals("pilgi")) {
+			bVo = pService.getPilgiDetail(detail);
+			model.addAttribute("pageTitle", "필기");
+			model.addAttribute("yList", pService.getYeongwanList(detail));
+		} else if(board.equals("jilmun")) {
+			bVo = jmService.getJilmunDetail(detail);
+			model.addAttribute("pageTitle", "질문");
+		} else if(board.equals("jayu")) {
+			bVo = jyService.getJayuDetail(detail);
+			model.addAttribute("pageTitle", "자유");
+		}
+		
+		String str = "";
+		String[] array = null;
+		
+		if(bVo.getSubjectCode()!=null) {
+			str = bVo.getSubjectCode().substring(1, bVo.getSubjectCode().length() - 1); // 대괄호 제거
+			array = str.split(","); // 쉼표를 기준으로 분리
+
+			for (int i = 0; i < array.length; i++) {
+			    array[i] = array[i].replaceAll("\"", "").trim(); // 큰따옴표 제거 및 공백 제거
+			}
+		}
+
+		model.addAttribute("bVo", bVo);
+		model.addAttribute("subArr", array);
+		return "communityDetails";
+	}
+	
+	@RequestMapping(value="/commynityWriteForm.do", method=RequestMethod.GET)
+	public String commynityWriteForm(Model model, HttpSession session, String id) {
 		String board = (String)session.getAttribute("community");
+		log.info("@@@@@@@@@@@@@@@ 커뮤니티 새 글 작성 Form 이동 : board {}, boardId {}", board, id);
+		model.addAttribute("title","커뮤니티");
+		
+		UserProfileVo userInfo = (UserProfileVo)session.getAttribute("userInfo");
+		if(userInfo == null) {
+			userInfo = new UserProfileVo();
+			userInfo.setUserAccountId("TMTD0");
+		}
+		
 		if(board.equals("pilgi")) {
 			model.addAttribute("pageTitle", "필기");
 			model.addAttribute("classVo",pService.getPilgiClassDetail(id)); 
