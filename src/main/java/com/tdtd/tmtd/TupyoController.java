62263934f5a62fec7280c3038d4f7898432c3722
dahyun623 <danielanderic@gmail.com>
2023-10-04 17:20:45 +0900

    TDTD-217 클래스 상태 변경 수정

--- src/main/java/com/tdtd/tmtd/TupyoController.java
+++ src/main/java/com/tdtd/tmtd/TupyoController.java
@@ -352,7 +352,7 @@ public class TupyoController {
 				
 				Map<String, Object> classStatusMap = new HashMap<String, Object>();
 				classStatusMap.put("clasId", clasId);
-				classStatusMap.put("clasStatus", "모집완료");
+				classStatusMap.put("clasStatus", "매칭완료");
 				service.updateClassStatus(classStatusMap);
 				
 				LocalDate currentDate = LocalDate.now();
