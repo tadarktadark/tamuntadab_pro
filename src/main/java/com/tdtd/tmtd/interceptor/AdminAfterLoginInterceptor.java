6e2d4f1501cc4f268b0d50e029d8e870507f97e0
wjddns7828 <wjddns7828>
2023-10-06 01:38:22 +0900

    관리자 권한 인터셉터 추가

--- src/main/java/com/tdtd/tmtd/interceptor/AdminAfterLoginInterceptor.java
+++ src/main/java/com/tdtd/tmtd/interceptor/AdminAfterLoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.tdtd.tmtd.interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,16 +9,15 @@ import org.springframework.web.servlet.AsyncHandlerInterceptor;
 
 import com.tdtd.tmtd.vo.AdminVo;
 
-import lombok.extern.slf4j.Slf4j;
-
 public class AdminAfterLoginInterceptor implements AsyncHandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		AdminVo adminInfo = (AdminVo) request.getSession().getAttribute("adminInfo");
 		if(adminInfo == null) {
-			request.getSession().invalidate();
-			response.sendRedirect("./admin.do");
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('다시 로그인 해주세요.');location.href='./admin.do';</script>");
 			return false;
 		}
 		return AsyncHandlerInterceptor.super.preHandle(request, response, handler);
---
+++ src/main/java/com/tdtd/tmtd/interceptor/AdminAuthTInterceptor.java
@@ -0,0 +1,31 @@
+package com.tdtd.tmtd.interceptor;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
+
+import com.tdtd.tmtd.vo.AdminVo;
+
+public class AdminAuthTInterceptor implements AsyncHandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		AdminVo adminInfo = (AdminVo) request.getSession().getAttribute("adminInfo");
+		if(adminInfo != null) {
+			if(!adminInfo.getAdprAuth().equals("T")) {
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				out.println("<script>alert('권한이 없습니다');location.href='./adminMain.do';</script>");
+				return false;
+			}else {
+				return AsyncHandlerInterceptor.super.preHandle(request, response, handler);
+			}
+		}else {
+			response.sendRedirect("../main.do");
+			return false;
+		}
+	}
+}
